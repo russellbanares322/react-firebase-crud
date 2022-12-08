f8c14c0a757ae0863e734de451affd58bcc5afc3
russellbanares322 <banaresrussell11@gmail.com>
2022-12-08 10:07:21 +0800

    chore: add firebase config to .env file

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -3,13 +3,22 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
+const {
+  REACT_APP_API_KEY,
+  REACT_APP_AUTH_DOMAIN,
+  REACT_APP_PROJECT_ID,
+  REACT_APP_STORAGE_BUCKET,
+  REACT_APP_MESSAGING_ID,
+  REACT_APP_API_ID,
+} = process.env;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyDRKnl5E4M0zlBAQv54EGDCLm2dPnGpM6E",
-  authDomain: "fir-crud-e0fd8.firebaseapp.com",
-  projectId: "fir-crud-e0fd8",
-  storageBucket: "fir-crud-e0fd8.appspot.com",
-  messagingSenderId: "628807091480",
-  appId: "1:628807091480:web:38b8aebbb8f8ffd890ca6d",
+  apiKey: REACT_APP_API_KEY,
+  authDomain: REACT_APP_AUTH_DOMAIN,
+  projectId: REACT_APP_PROJECT_ID,
+  storageBucket: REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: REACT_APP_MESSAGING_ID,
+  appId: REACT_APP_API_ID,
 };
 
 const app = initializeApp(firebaseConfig);
